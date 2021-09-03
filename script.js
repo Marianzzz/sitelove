96c5e5ecc2631770958e420189f08704573fd330
Marianzzz <90037057+Marianzzz@users.noreply.github.com>
2021-09-03 18:08:32 +0300

    чина

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:true,
+    dots:true,
+    adaptiveHeight:true,
+    slidesToShow:2,
+    slidesToScroll:2,
+    infinite:false,
+    draggable:false,
+    swipe:false,
+});
+});
